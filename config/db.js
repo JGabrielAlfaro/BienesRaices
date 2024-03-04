5e38ef004634264006080426d76279578ca1d82d
Gabriel Alfaro <gabriel.alfaro.cruz@gmail.com>
2024-03-03 20:45:22 -0600

    Mejora en las variables de ambiente

--- config/db.js
+++ config/db.js
@@ -3,6 +3,16 @@ import { config } from 'dotenv';
 
 config({ path: '.env' });
 
+// Determinar el entorno actual (puede ser a través de una variable de entorno NODE_ENV)
+const env = process.env.NODE_ENV || 'desarrollo';
+
+// Cargar las variables de entorno según el entorno actual
+if (env === 'desarrollo') {
+    config({ path: '.env.desarrollo' });
+} else if (env === 'produccion') {
+    config({ path: '.env.produccion' });
+}
+
 const db = new Sequelize(process.env.DB_NOMBRE, process.env.DB_USER, process.env.DB_PASS ?? '', {
     host:  process.env.DB_HOST,
     port: process.env.DB_PORT,
